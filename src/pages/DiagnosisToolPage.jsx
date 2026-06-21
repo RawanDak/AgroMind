26c182f762f260f86d2364bec72f43f921a4f1c8
RawanDak <rawandaknan@gmail.com>
2026-06-21 14:01:30 +0300

    Update diagnosis endpoint for EC2

--- src/pages/DiagnosisToolPage.jsx
+++ src/pages/DiagnosisToolPage.jsx
@@ -37,7 +37,7 @@ function DiagnosisToolPage() {
     try {
       const token = localStorage.getItem("token");
 
-      const response = await fetch("http://localhost:8000/diagnose", {
+      const response = await fetch("http://98.80.119.7:8000/diagnose", {
         method: "POST",
         headers: {
           Authorization: `Bearer ${token}`,
